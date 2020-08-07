4acbc561f21220b9b34beec5a8b400840f437a8b
Alex Lewis <alexlewis374@gmail.com>
2020-08-07 12:44:43 +0100

    Add separate Card element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import Card from './components/card'
+import Card from './components/cards'
 
 function App() {
   return (
--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = () => {
   return (
     <div>
-      <p>Hello</p>
+      <p>I'm a Card</p>
     </div>
   )
 }
