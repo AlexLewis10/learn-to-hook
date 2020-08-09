1e49d56867a16bf6253c34a0a3acec091d17b5cc
Alex Lewis <alexlewis374@gmail.com>
2020-08-09 15:57:49 +0100

    Make lines less than 80 charecters

--- src/components/cards.js
+++ src/components/cards.js
@@ -7,7 +7,9 @@ const Cards = () => {
 
   useEffect(() => {
     if (cards === null) {
-    Axios.get(`https://deckofcardsapi.com/api/deck/5fm1fdsovocs/draw/?count=1`)
+    Axios.get(
+      `https://deckofcardsapi.com/api/deck/5fm1fdsovocs/draw/?count=1`
+      )
     .then(response => {
       const result = response.data.cards
       setCards(result)
@@ -20,7 +22,9 @@ const Cards = () => {
 
   const displayCards = () => {
     if (cards) {
-      return cards.map((card) => { return <Card key={card.value} card={card.value}/> })
+      return cards.map((card) => { 
+        return <Card key={card.value} card={card.value}/> 
+      })
     }
   }
 
